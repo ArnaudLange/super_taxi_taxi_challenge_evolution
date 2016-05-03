06d71558f3f563a91f661adf5ddd5082f5da9522
Quentin MAISONNEUVE <quentin.maisonneuve1@etu.univ-nantes.fr>
2016-05-03 23:09:12 +0200

    Ajout classe jeu

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -1,7 +1,21 @@
 package jeu;
 
+import carte.Carte;
+
+import java.util.List;
+
 /**
  * Created by swag on 03/05/16.
  */
-public class Jeu {
+public class Jeu
+{
+    private List<Joueur> listeJoueurs;
+    private Carte carte;
+    private Joueur gagnant ;
+
+    public Jeu(List<Joueur> listeJoueurs, int largeurCarte, int longueurCarte)
+    {
+        this.listeJoueurs = listeJoueurs;
+        this.carte = new Carte(longueurCarte, largeurCarte);
+    }
 }
