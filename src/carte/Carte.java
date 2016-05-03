ecfcf22d27dee1111183320a146eb7c285d617be
Quentin MAISONNEUVE <quentin.maisonneuve1@etu.univ-nantes.fr>
2016-05-03 23:27:16 +0200

    maj carte

--- src/carte/Carte.java
+++ src/carte/Carte.java
@@ -1,17 +1,48 @@
 package carte;
 
+import java.util.Arrays;
+
 /**
  * Created by jimmy on 02/05/16.
  */
-public class Carte
-{
+public class Carte {
+
+
 
     private int longueur; //nb de cases en abcisse
-    private int largeur; // nb de cases en ordonnée
 
-    public Carte(int longueur, int largeur)
-    {
+    public int getLargeur() {
+        return largeur;
+    }
+
+    public int getLongueur() {
+        return longueur;
+    }
+
+    private int largeur; // nb de cases en ordonnée
+    private Case[][] tableau;
+
+    public Case[][] getTableau() {
+        return tableau;
+    }
+
+    public void setLongueur(int longueur) {
         this.longueur = longueur;
+    }
+
+    public void setLargeur(int largeur) {
         this.largeur = largeur;
+    }
+
+    @Override
+    public String toString() {
+        return "Carte{" +
+                "tab=" + Arrays.toString(tableau) +
+                '}';
+    }
+
+
+    public Carte(Case [][] tableau){
+        this.tableau = tableau;
     }
 }
