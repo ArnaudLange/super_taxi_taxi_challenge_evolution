0364620a35c58a6bea1c3715f08201e7f1d8cf88
Jimmy <jimmy.leblanc@etu.univ-nantes.fr>
2016-05-16 22:14:34 +0200

    timer serveur

--- src/jeu/Controlleur.java
+++ src/jeu/Controlleur.java
@@ -2,7 +2,7 @@ package jeu;
 
 import carte.Carte;
 import carte.InterpreteurCarte;
-import jdk.nashorn.internal.scripts.JO;
+//import jdk.nashorn.internal.scripts.JO;
 import reseau.Serveur;
 
 import java.io.File;
@@ -17,7 +17,6 @@ public class Controlleur {
 
     public static void main(String[] args)
     {
-
         File fichierCarte = new File("src/carte/carte001.txt"); // on initialise le fichier texte de la carte
         Carte carte = InterpreteurCarte.Interpreter(fichierCarte);
         Jeu jeu = new Jeu(new ArrayList<Joueur>(), carte);
--- src/reseau/Serveur.java
+++ src/reseau/Serveur.java
@@ -18,11 +18,12 @@ public class Serveur {
             listeningSocket = new ServerSocket(2009);
             System.out.println("Serveur à l'écoute sur le port " + listeningSocket.getLocalPort());
 
-            while(true)
+            while(listeJoueur.size() < 2)
             {
                 // Création du prochain joueur relié au client qui se connectera
                 Joueur joueur = new Joueur();
                 listeJoueur.add(joueur);
+
 
                 // Attente de la connexion à un client
                 Socket socketClient = listeningSocket.accept();
@@ -31,6 +32,27 @@ public class Serveur {
                 Thread t = new Thread(new ConnexionClient(socketClient, joueur));
                 t.start();
             }
+            System.out.println(" On a 2 joueurs ! ");
+            long time = System.currentTimeMillis();//temps de depart
+            long fin=time + 5000;//temps de fin
+            long duree = (fin - time);
+            System.out.println("durée = "+ duree/1000);
+            while (fin > System.currentTimeMillis()){
+                System.out.println(" On rentre dans le timer");
+                // Création du prochain joueur relié au client qui se connectera
+                Joueur joueur = new Joueur();
+                listeJoueur.add(joueur);
+
+
+                // Attente de la connexion à un client
+                Socket socketClient = listeningSocket.accept();
+
+                // Lancement du thread qui gèrera le client avec l'objet Joueur en paramètre
+                Thread t = new Thread(new ConnexionClient(socketClient, joueur));
+                t.start();
+            }
+            System.out.println("On sort du timer, la partie va commencer");
+
         }
         catch (IOException e)
         {
