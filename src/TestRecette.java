ac6eb28e343fcb0a136500f162d0ebbe2d4fee4e
Arnaud Langelier <arnaud.langelier@etu.univ-nantes.fr>
2016-06-03 03:18:41 +0200

    ajout de trucs pour test

---
+++ src/TestRecette.java
@@ -0,0 +1,12 @@
+import jeu.ControleurTest;
+
+/**
+ * Created by Cokral on 26/05/2016.
+ */
+public class TestRecette {
+
+    public static void main(String[] args) {
+        ControleurTest contro = new ControleurTest();
+        contro.lancerJeu();
+    }
+}
